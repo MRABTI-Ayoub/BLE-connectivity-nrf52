b835408cf473585582c1d04e7fe582f96bcbf88e
MRABTI Ayoub <mrabtiayoub345@gmail.com>
2024-08-15 00:50:35 -0700

    Modifications pour la branche peripheral

---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_gls_pca10040_s132/sensorsim.d
@@ -0,0 +1,5 @@
+Output/Release/Obj/ble_app_gls_pca10040_s132/sensorsim.o: \
+ C\:\Users\MRABTI\ Ayoub\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.c \
+ C\:\Users\MRABTI\ Ayoub\Desktop\nRF5_SDK_17.1.0_ddde560\components\libraries\sensorsim\sensorsim.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdbool.h
---
+++ pca10040/s132/ses/Output/Release/Obj/ble_app_gls_pca10040_s132/ses_startup_nrf_common.d
@@ -0,0 +1,2 @@
+Output/Release/Obj/ble_app_gls_pca10040_s132/ses_startup_nrf_common.o: \
+ C\:\Users\MRABTI\ Ayoub\Desktop\nRF5_SDK_17.1.0_ddde560\modules\nrfx\mdk\ses_startup_nrf_common.s
